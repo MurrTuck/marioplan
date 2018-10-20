9c7eb561fcf7630d863411dd4e77d0848798a930
MurrTuck <murrtuck@gmail.com>
2018-10-19 23:51:29 -0600

    Just wanted to get something uploaded to GitHub

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -3,7 +3,7 @@ const initState = {
         { id: '1', title: 'help me find peach', content: 'blah blah blah' },
         { id: '2', title: 'collect all the stars', content: 'blah blah blah' },
         { id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah' },
-        { id: '4', title: 'reach the last moon', content: 'blah blah blah' },
+        { id: '4', title: 'reach the last moon', content: 'blah' },
     ]
 }
 
