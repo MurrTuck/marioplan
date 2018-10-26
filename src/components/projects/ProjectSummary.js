5c48792103376a6b79d027eae7635f6d144df303
MurrTuck <murrtuck@gmail.com>
2018-10-25 18:37:14 -0600

    Formatting Dates with Moment

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import moment from 'moment'
 
 const ProjectSummary = ({ project }) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>Posted by Murr Tuck</p>
-                <p className='grey-text'>15th October, 11pm</p>
+                <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+                <p className='grey-text'>{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,17 +1,18 @@
 export const createProject = (project) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
-        //make async call to database
+    return (dispatch, getState, { getFirestore }) => {
         const firestore = getFirestore();
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Bigg',
-            authorLastName: 'Murr',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => {
-            dispatch({ type: 'CREATE_PROJECT', project })
+            dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
         }).catch(err => {
-            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+            dispatch({ type: 'CREATE_PROJECT_ERROR' }, err);
         });
     }
-}
+};
