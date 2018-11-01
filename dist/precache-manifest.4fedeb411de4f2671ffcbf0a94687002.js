eedba4ce67b4af63fee1ad06466db686a49b88cf
MurrTuck <murrtuck@gmail.com>
2018-11-01 17:33:57 -0600

    Finishing up

---
+++ dist/precache-manifest.4fedeb411de4f2671ffcbf0a94687002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6531af5a002ee8e329b6",
+    "url": "/static/js/main.6531af5a.chunk.js"
+  },
+  {
+    "revision": "06321270dd5abde6655d",
+    "url": "/static/js/1.06321270.chunk.js"
+  },
+  {
+    "revision": "6531af5a002ee8e329b6",
+    "url": "/static/css/main.292758ab.chunk.css"
+  },
+  {
+    "revision": "5ed3c53ec094943ceb2fa52157291bea",
+    "url": "/index.html"
+  }
+];
